fa3f11471c7bcabc7bae73f017ff4628e610607e
Natyari Vargas Oconitrillo <naryari@gmail.com>
2021-10-20 21:07:25 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -3,3 +3,21 @@ import re
 import sys
 from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
+
+@app.context_prrocessor
+def inject_load():
+    if sys.platform.startswith('linux'):
+        with open('/proc/loadavg', 'rt') as f:
+            load = f.read().split()[0:3]
+    else:
+        load = [int(random.random() * 100) / 100 for _ in range(3)]
+    return{'load1': load[0], 'load5': load5[1], 'load5': load[2]}
